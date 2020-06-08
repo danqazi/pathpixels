f47084ada595ebaa525736cf0f28418651084787
Daniel Qazi <danqazi03@gmail.com>
2020-06-08 03:54:10 -0700

    created script for image processing, test version2

--- pathologypixels/management/commands/process_image.py
+++ pathologypixels/management/commands/process_image.py
@@ -2,8 +2,6 @@ import sys
 import os
 import pyvips
 
-vipshome = 'C:\\Users\danqa\\vips-dev-8.9\\bin'
-os.environ['PATH'] = vipshome + ';' + os.environ['PATH']
 path = os.getcwd()
 images = os.listdir(path)
 for image in images:
