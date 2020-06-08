0571058b2f8fc602a97780317bd6b4aae8044ce7
Jacob <jacobdspector@gmail.com>
2020-06-08 12:01:17 -0400

    Working on static files

--- pathologypixels/settings.py
+++ pathologypixels/settings.py
@@ -150,9 +150,8 @@ STATICFILES_DIRS = (STATIC_DIR,)
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 
-
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_S3_FILE_OVERWRITE = False
 
